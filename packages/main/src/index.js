69eb08c853b2e486534475d6554f6bc4c59afc88
Daniele Rolli <danielerolli@proton.me>
2024-01-01 11:29:24 +0100

    🐞 Removed autoUpdater

--- packages/main/src/index.js
+++ packages/main/src/index.js
@@ -3,7 +3,6 @@ import { ipcMain } from 'electron-better-ipc';
 import { join, normalize } from 'path';
 import { URL } from 'url';
 import { remove, readJson, ensureDir, copy, outputJson, pathExistsSync } from 'fs-extra';
-import { autoUpdater } from 'electron-updater';
 import store from './store';
 
 const isSingleInstance = app.requestSingleInstanceLock();
@@ -21,7 +20,6 @@ app.disableHardwareAcceleration();
  */
 const env = import.meta.env;
 
-
 // Install "Vue.js devtools"
 if (env.MODE === 'development') {
   app.whenReady()
@@ -38,31 +36,23 @@ let mainWindow = null;
 
 const createWindow = async () => {
   mainWindow = new BrowserWindow({
-    show: false, // Use 'ready-to-show' event to show window
+    show: false,
     width: 950,
     height: 600,
     webPreferences: {
       preload: join(__dirname, '../../preload/dist/index.cjs'),
-      contextIsolation: env.MODE !== 'test',   // Spectron tests can't work with contextIsolation: true
-      enableRemoteModule: env.MODE === 'test', // Spectron tests can't work with enableRemoteModule: false
+      contextIsolation: env.MODE !== 'test',
+      enableRemoteModule: env.MODE === 'test',
     },
   });
 
   mainWindow.setMenuBarVisibility(false);
 
-  /**
-   * If you install `show: true` then it can cause issues when trying to close the window.
-   * Use `show: false` and listener events `ready-to-show` to fix these issues.
-   *
-   * @see https://github.com/electron/electron/issues/25012
-   */
   mainWindow.on('ready-to-show', () => {
     mainWindow?.show();
 
     if (env.MODE === 'development') {
       mainWindow?.webContents.openDevTools();
-    } else {
-      autoUpdater.checkForUpdatesAndNotify();
     }
   });
 
@@ -74,35 +64,25 @@ const createWindow = async () => {
     shell.openExternal(url);
   });
 
-  /**
-   * URL for main window.
-   * Vite dev server for development.
-   * `file://../renderer/index.html` for production and test
-   */
   const pageUrl = env.MODE === 'development'
     ? env.VITE_DEV_SERVER_URL
     : new URL('../renderer/dist/index.html', 'file://' + __dirname).toString();
 
-
   await mainWindow.loadURL(pageUrl);
 };
 
-
 app.on('second-instance', () => {
-  // Someone tried to run a second instance, we should focus our window.
   if (mainWindow) {
     if (mainWindow.isMinimized()) mainWindow.restore();
     mainWindow.focus();
   }
 });
 
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
 
 app.whenReady()
   .then(async () => {
@@ -119,15 +99,6 @@ app.whenReady()
     await createWindow();
   })
   .catch((e) => console.error('Failed create window:', e));
-
-
-// Auto-updates
-if (env.PROD) {
-  app.whenReady()
-    .then(() => import('electron-updater'))
-    .then(({ autoUpdater }) => autoUpdater.checkForUpdatesAndNotify())
-    .catch((e) => console.error('Failed check updates:', e));
-}
 
 ipcMain.answerRenderer('app:info', () => ({
   name: app.getName(),
