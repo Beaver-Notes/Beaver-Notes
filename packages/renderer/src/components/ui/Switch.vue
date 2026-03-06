38756cdcb37a393a102d311a11bfd7315216819f
Daniele Rolli <danielerolli@proton.me>
2026-03-06 15:23:32 +0100

    fix: ui

--- packages/renderer/src/components/ui/Switch.vue
+++ packages/renderer/src/components/ui/Switch.vue
@@ -9,7 +9,7 @@
       @change="handleChange"
     />
     <div
-      class="peer h-6 w-11 rounded-full border bg-neutral-200 dark:bg-[#353333] after:absolute after:left-[2px] rtl:after:right-[22px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-neutral-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary peer-checked:after:translate-x-full rtl:peer-checked:after:border-white peer-focus:ring-green-300"
+      class="peer h-6 w-11 rounded-full border bg-neutral-200 dark:bg-[#353333] transition-colors after:absolute after:left-[2px] rtl:after:right-[22px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-neutral-300 after:bg-white after:transition-transform after:content-[''] peer-checked:bg-primary peer-checked:after:translate-x-full rtl:peer-checked:after:border-white peer-focus:ring-1 peer-focus:ring-secondary"
     ></div>
   </label>
 </template>
