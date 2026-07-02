444282a7f871018db60011c78692af644a5eb907
Daniele-rolli <danielerolli@proton.me>
2026-07-02 21:04:49 +0200

    feat: add android

---
+++ src-tauri/gen/android/app/src/main/java/com/danielerolli/beaver_notes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.danielerolli.beaver_notes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
