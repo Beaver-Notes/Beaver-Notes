e3aacfcd5780343e3027769fd2c426dfcbb04e2a
Daniele-rolli <danielerolli@proton.me>
2026-08-16 21:54:40 +0200

    build(android): bump Kotlin Gradle plugin to 2.1.20 for stdlib 2.1 compatibility
    Signed-off-by: Daniele-rolli <danielerolli@proton.me>

--- src-tauri/gen/android/build.gradle.kts
+++ src-tauri/gen/android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.11.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
     }
 }
 
